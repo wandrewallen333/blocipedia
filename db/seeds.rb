7a9e973d9179a7bff10f8d255aec62adf58694d3
Andrew Allen <w.andrewallen@gmail.com>
2018-01-03 18:55:04 -0600

    Blocipedia Seeding Dat 6: Complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,42 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+ 10.times do
+   User.create!(
+     email: Faker::Internet.email,
+     password: 'helloworld',
+   )
+ end
+
+ users = User.all
+
+ 100.times do
+   wiki = Wiki.create!(
+     user: users.sample,
+     title: Faker::StarWars.character,
+     body: Faker::StarWars.quote
+   )
+ end
+
+ wikis = Wiki.all
+
+ admin = User.create!(
+   email: 'admin@example.com',
+   password: 'password',
+   role: 'admin'
+ )
+
+ premium = User.create!(
+   email: 'premium@example.com',
+   password: 'password',
+   role: 'premium'
+ )
+
+ standard = User.create!(
+   email: 'standard@example.com',
+   password: 'password',
+   role: 'standard'
+ )
+
+ puts "Seed finished"
+ puts "#{User.count} users created"
+ puts "#{Wiki.count} posts created"
