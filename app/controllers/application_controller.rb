c582bc4527d6e350d9671053db8b3fcb07c45956
Andrew Allen <w.andrewallen@gmail.com>
2018-01-04 12:09:59 -0600

    Still acting weird but minor fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  include Pundit 
+  include Pundit
   protect_from_forgery with: :exception
-  before_action :authenticate_user!
+  #before_action :authenticate_user!
 end
