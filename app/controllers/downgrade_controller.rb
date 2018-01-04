c704875174ee776022682d81f160d767258a25d8
Andrew Allen <w.andrewallen@gmail.com>
2018-01-04 11:49:13 -0600

    Problems with private wikis

--- app/controllers/downgrade_controller.rb
+++ app/controllers/downgrade_controller.rb
@@ -5,6 +5,7 @@ class DowngradeController < ApplicationController
   def create
     current_user.update_attribute(:role, 0)
     #current_user.update_attribute(:role, 1)
+    current_user.going_public
 
     flash[:notice] = "You have successfully downgraded your account."
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,11 @@ class User < ApplicationRecord
 
   has_many :wikis, dependent: :destroy
 
-  before_save { self.role ||= :standard }
+  #before_save { self.role ||= :standard }
+
+  def going_public
+    self.wikis.each { |wiki| puts wiki.publicize }
+  end 
 
   enum role: [:standard, :premium, :admin]
 end
