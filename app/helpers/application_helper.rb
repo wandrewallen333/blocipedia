d0e3b4bd95be5857c19cc4fd193ef57884a9d97a
Andrew Allen <w.andrewallen@gmail.com>
2018-01-04 14:05:11 -0600

    Markdown Complete

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,22 +1,6 @@
 module ApplicationHelper
-  def markdown(text)
-       options = {
-         filter_html:     true,
-         hard_wrap:       true,
-         link_attributes: { rel: 'nofollow', target: "_blank" },
-         space_after_headers: true,
-         fenced_code_blocks: true
-       }
-
-       extensions = {
-         autolink:           true,
-         superscript:        true,
-         disable_indented_code_blocks: true
-       }
-
-       renderer = Redcarpet::Render::HTML.new(options)
-       markdown = Redcarpet::Markdown.new(renderer, extensions)
-
-       markdown.render(text).html_safe
-     end
-   end
+  def markdown(body)
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+    markdown.render(body).html_safe
+  end
+end
